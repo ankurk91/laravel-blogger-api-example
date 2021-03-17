4d0ebbed78a4846c3cc5226694a616dc9ef8e43a
ankurk91 <pro.ankurk1@gmail.com>
2021-03-17 10:41:35 +0530

    Make a test user available all the time in dev

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -14,6 +14,20 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        $users = [
+            [
+                'email' => env('TEST_USER_EMAIL', 'ankurk91@example.com'),
+                'name' => env('TEST_USER_NAME', 'ankurk91'),
+            ],
+        ];
+
+        foreach ($users as $record) {
+            $user = new User();
+            $user->fill($record);
+            $user->setAttribute('password', bcrypt('password@123'));
+            $user->save();
+        }
+
         User::factory(rand(5, 10))->create();
     }
 }
